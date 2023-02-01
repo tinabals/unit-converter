3441f393379be066b2d96702ac3a4d5e58a622a7
Damilola Balogun <balogundamilola.f@gmail.com>
2023-02-01 19:24:09 +0100

    functionalities addes
    added the functionalities to convert a number to length, volume and mass in both imperial and metric.

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+const inputNumber = document.getElementById("number-input")
+const convertBtn = document.getElementById("convert-btn")
+let lengthResult = document.getElementById("length-result")
+let volumeResult = document.getElementById("volume-result")
+let massResult = document.getElementById("mass-result")
+
+
+
+function convert(input){
+   length(input)
+   volume(input)
+   mass(input)
+  inputNumber.value = " "
+
+}
+
+function length(input){
+   let feet= (input * meterToFeet).toFixed(3);
+   let metres =  (input / meterToFeet).toFixed(3);
+   lengthResult.textContent = `${input} meters = ${feet} feet | ${input} feets = ${metres} meters`
+}
+function volume(input){
+   let gallons = (input * literToGallon).toFixed(3);
+   let liters =  (input / literToGallon).toFixed(3);
+   volumeResult.textContent = `${input} litres = ${gallons} gallons | ${input} gallons = ${liters} litres`
+}
+function mass(input){
+   let klbs= (input * kiloToPound).toFixed(3);
+   let pounds =  (input / kiloToPound).toFixed(3);
+   massResult.textContent = `${input} klbs = ${klbs} pounds | ${input} pounds = ${pounds} klbs`
+}
+
+
+
+convertBtn.addEventListener('click', () => convert(inputNumber.value))
